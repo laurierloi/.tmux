a8eb55e0261cfbee07f56f9a54d8a382c87a3ad1
Laurier Loiselle <lal@xiphos.com>
2024-05-06 17:47:14 -0400

    setup: force simlink

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P  )"
 
-ln -s ${SCRIPTPATH}/.tmux.conf $HOME/.tmux.conf
-ln -s ${SCRIPTPATH}/.tmux.conf.local $HOME/.tmux.conf.local
+ln -fs ${SCRIPTPATH}/.tmux.conf $HOME/.tmux.conf
+ln -fs ${SCRIPTPATH}/.tmux.conf.local $HOME/.tmux.conf.local
